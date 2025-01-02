9072d11cca823ffc807f9b01c3542fecea1b1891
Abdelilah Elgallati <abdelilah.elgallati6@gmail.com>
2025-01-02 12:58:37 +0100

    Fix some error

--- backend/models/messageModel.js
+++ backend/models/messageModel.js
@@ -1,25 +1,3 @@
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
-
-// const MessageSchema = new Schema(
-//   {
-//     productId: { type: Schema.Types.ObjectId, ref: "Product", required: true },
-//     messages: [
-//       {
-//         sender: { type: String, required: true },
-//         receiver: { type: String, required: true },
-//         content: { type: String, required: true },
-//         timestamp: { type: Date, default: Date.now },
-//       },
-//     ],
-//   },
-//   { timestamps: true }
-// );
-
-// const Message = mongoose.model('Message', MessageSchema);
-
-// module.exports = Message;
-
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
@@ -49,21 +27,6 @@ const messageSchema = new mongoose.Schema({
   timestamps: true 
 });
 
-// Add indexes for better query performance
-messageSchema.index({ senderId: 1, receiverId: 1, productId: 1 });
-messageSchema.index({ createdAt: -1 }); // For sorting by date
-
-// Add a method to mark message as delivered
-messageSchema.methods.markAsDelivered = function() {
-  this.status = 'delivered';
-  return this.save();
-};
-
-// Add a method to mark message as read
-messageSchema.methods.markAsRead = function() {
-  this.readAt = new Date();
-  return this.save();
-};
 
 const Message = mongoose.model("Message", messageSchema);
 
